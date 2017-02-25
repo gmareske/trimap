e4f9fe7b841f0b9e19e1157a2fce6d3cbb43ffcf
Griffin Mareske <gmareske@gmail.com>
2017-02-24 23:52:20 -0600

    fix graph probably for good

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var   ctx    = canvas.getContext("2d");
 
 const C_HEIGHT = canvas.height;
 const C_WIDTH  = canvas.width;
-const N_LINES  = 10;
+const N_LINES  = 25;
 
 ctx.save();
 
@@ -15,18 +15,36 @@ var drawLine  = function(x1, y1, x2, y2) {
     ctx.closePath();
 }
 const drawGraph = function() {
-    let mult = C_HEIGHT / N_LINES;
+    let mult = C_HEIGHT / N_LINES ;
     ctx.strokeStyle = "rgb(222, 222, 222)";
+    drawLine(0,0, C_HEIGHT, 0);
+    drawLine(0,C_HEIGHT,C_HEIGHT,C_HEIGHT);
     for (i = 0; i <= N_LINES * 2; i++) {
-	drawLine( 0.5 * mult * i, 0, 0.5 * mult * i, C_HEIGHT);
+	drawLine(0.5 * mult * i, 0, 0.5 * mult * i, C_HEIGHT);
     }
-    for (i = 0; i <= N_LINES; i++) {
+    
+    for (i = 0; i <= N_LINES ; i++) {
+	drawLine(mult*i,0, 0, mult*0.5*i);
+	drawLine(C_HEIGHT- mult* i,0,C_HEIGHT, mult*0.5*i);
+	drawLine(mult*i, C_HEIGHT, 0, C_HEIGHT-mult*0.5*i);
+	drawLine(C_HEIGHT - mult *i, C_HEIGHT, C_HEIGHT, C_HEIGHT - mult*0.5*i);
+
+    }
+
+    for (i = 1; i < N_LINES; i++){
+	drawLine(0, C_HEIGHT*0.5+ mult*0.5*i, C_HEIGHT, mult*0.5*i);
+	drawLine(0, mult*0.5*i, C_HEIGHT, C_HEIGHT*0.5 + mult*0.5*i);	
+    }
+    
+  /*  mult = mult / 2;
+    for (i = 0; i <= N_LINES * 4; i++) {
+	
 	drawLine(mult * i, 0, mult * i, C_HEIGHT);
 	drawLine(0, mult * i, mult * i, 0);
 	drawLine(mult * i, C_HEIGHT, C_HEIGHT, mult * i);
 	drawLine(C_HEIGHT - (mult * i), 0, C_HEIGHT, mult * i);
 	drawLine(0, C_HEIGHT - (mult * i), mult * i, C_HEIGHT);
-    }
+    }*/
 }
 
 drawGraph();
